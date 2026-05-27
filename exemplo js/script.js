01c81fb6fd60cc19849f0c4454454a2f4110227e
Luis Miguel dos Santos Ribeiro <luis46819746@aluno.ba.senac.br>
2026-05-27 16:42:33 -0300

    Adicionando js

---
+++ exemplo js/script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+    alert('Bem vindo ao javascript');
+    console.log("Clicou no botão");
+});
+
+
+//let idade;
+//idade = 20;
+//console.log(idade);
+
