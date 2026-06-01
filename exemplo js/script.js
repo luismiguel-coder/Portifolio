62785f6c1967eccf356315a31f95611b05e62319
Luis Miguel dos Santos Ribeiro <luis46819746@aluno.ba.senac.br>
2026-06-01 14:34:01 -0300

    att funções

--- exemplo js/script.js
+++ exemplo js/script.js
@@ -4,6 +4,18 @@ btn.addEventListener('click', function() {
     console.log("Clicou no botão");
 });
 
+const btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function(){
+    let titulo = prompt("Digite o novo titulo: "); 
+    document.getElementById("titulo").innerHTML = titulo;
+    console.log(`o titulo foi alterado para ${titulo}`);
+})
+
+
+//let titulo = document.getElementById("titulo");
+
+//titulo.innerHTML = "Mudança com JS";
+
 
 //let idade;
 //idade = 20;
