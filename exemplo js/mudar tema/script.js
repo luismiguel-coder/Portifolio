7fd3ad7280ab6ec185114af4c8d05391783c1740
Luis Miguel dos Santos Ribeiro <luis46819746@aluno.ba.senac.br>
2026-06-01 16:23:30 -0300

    ATT ADIÇÃO DE BTN NO MUDAR TEMA

--- exemplo js/mudar tema/script.js
+++ exemplo js/mudar tema/script.js
@@ -1,12 +1,25 @@
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
 
 btn1.addEventListener('click',  function(){
     document.body.style.backgroundColor = 'black';
     document.body.style.color = 'white';
 });
 
+
 btn2.addEventListener('click', function(){
     document.body.style.backgroundColor = 'white';
     document.body.style.color = 'black';
+});
+
+
+btn3.addEventListener('click', function(){
+    if(document.body.style.backgroundColor === 'black'){
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    } else{
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    }
 });
