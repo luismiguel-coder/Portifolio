e04528e13304e8a542a6558a3d64e1f9eb08a0c9
Luis Miguel dos Santos Ribeiro <luis46819746@aluno.ba.senac.br>
2026-06-01 15:14:03 -0300

    att pasta nova

---
+++ exemplo js/MIni projeto- contador/script.js
@@ -0,0 +1,16 @@
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", function() {
+    let contador = document.getElementById("h1");
+    contador.textContent = parseInt(contador.textContent) + 1;
+});
+
+const btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function() {
+    let contador = document.getElementById("h1");
+    contador.textContent = parseInt(contador.textContent) - 1;
+    
+    if (parseInt(contador.textContent) < 0) {
+        alert("O contador não pode ser negativo!");
+        contador.textContent = 0;
+    }
+});
--- exemplo js/script.js
+++ exemplo js/script.js
@@ -1,15 +1,21 @@
+
 const btn = document.getElementById('btn');
 btn.addEventListener('click', function() {
     alert('Bem vindo ao javascript');
-    console.log("Clicou no botão");
+    let titulo =  document.getElementById("titulo");
+    titulo.style.color = "blue";
+    console.log("o titulo foi alterado para azul");
 });
+
 
 const btn2 = document.getElementById("btn2");
 btn2.addEventListener("click", function(){
     let titulo = prompt("Digite o novo titulo: "); 
     document.getElementById("titulo").innerHTML = titulo;
     console.log(`o titulo foi alterado para ${titulo}`);
-})
+});
+
+
 
 
 //let titulo = document.getElementById("titulo");
@@ -20,4 +26,3 @@ btn2.addEventListener("click", function(){
 //let idade;
 //idade = 20;
 //console.log(idade);
-
